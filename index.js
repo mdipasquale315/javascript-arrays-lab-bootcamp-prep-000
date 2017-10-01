e8d0f6052e7d2bc92124ac4bc1efa590642a7213
Michael DiPasquale <mdipasquale315@gmail.com>
2017-10-01 00:20:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const app = "I don't do much."
-kittens = ['Milo', 'Otis', 'Garfield']
+var kittens = ['Milo', 'Otis', 'Garfield']
 function destructivelyAppendKitten(name){
    kittens['Ralph']
   return name.push
